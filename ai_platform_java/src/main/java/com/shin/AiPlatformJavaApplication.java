6b6cc289cfbcee6bdd74b1be73a561c10986411b
shin8085 <shin8085@foxmail.com>
2020-12-04 16:47:05 +0800

    项目初始化

---
+++ ai_platform_java/src/main/java/com/shin/AiPlatformJavaApplication.java
@@ -0,0 +1,13 @@
+package com.shin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPlatformJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiPlatformJavaApplication.class, args);
+    }
+
+}
---
+++ ai_platform_java/src/test/java/com/shin/AiPlatformJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiPlatformJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
